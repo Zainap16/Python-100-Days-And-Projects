bdb0c27493cc8e434109060e91def799c8e379ae
Ziah <122858207+Zainap16@users.noreply.github.com>
2025-01-23 08:11:30 +0000

    ye

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -11,19 +11,19 @@ len_word = len(ran_word)
  
 print(ran_word)   
 # take user input
-LIVES = 6
-display = ""
-while LIVES > 0:
-    guess = input("Enter character: ").lower()
-    LIVES -= 1
 
-for char in ran_word:
-    if char == guess:
-        display += char
-    else:
-        display += "_"
-            
-    print(display)       
+display = ""
+for lives in range(5):
+    guess = input("Enter character: ").lower()
+  
+
+    for char in ran_word:
+        if char == guess:
+            display += char
+        else:
+            display += "_"
+                
+        print(display)       
     
   
 # compare user input to word[ran_word]
