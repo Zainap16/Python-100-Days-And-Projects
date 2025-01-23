c965c569acc690056195c1025e6fe7307c6496dc
Ziah <122858207+Zainap16@users.noreply.github.com>
2025-01-23 06:41:17 +0000

    hangman

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -6,19 +6,24 @@ words = ["apple","pear","catty"]
 ran_word = random.choice(words)
 
 # put _ down for each letter
-under = ""
+
 len_word = len(ran_word)
-for underscore in range(len_word):
-    under += "_"
-print(under) 
+ 
 print(ran_word)   
 # take user input
 LIVES = 6
+display = ""
 while LIVES > 0:
-    guess = input("Enter character: ")
-    for char in ran_word:
-        if char == guess:
-            print("CORRECT")
-        
+    guess = input("Enter character: ").lower()
     LIVES -= 1
+
+for char in ran_word:
+    if char == guess:
+        display += char
+    else:
+        display += "_"
+            
+    print(display)       
+    
+  
 # compare user input to word[ran_word]
