7969e7add39eee8159338ae218111b1a2b3f8c87
Ziah <122858207+Zainap16@users.noreply.github.com>
2025-01-31 12:01:45 +0200

    sorta compete challe 3

---
+++ Day18/main1.py
@@ -0,0 +1,34 @@
+# draw a square with turtle garphos
+from turtle import Turtle, Screen
+import random
+timmy = Turtle()
+
+# for steps in range(100):
+#     for c in ('blue', 'red', 'green'):
+#         timmy.color(c)
+#         timmy.forward(steps)
+#         timmy.right(30)
+counter = 4
+
+
+# for _ in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
+c = ['blue', 'red', 'green','salmon1','coral','SkyBlue','OrangeRed']
+for stop in range(counter):
+    temp = (360 / counter)
+    ac = random.choice(c)
+    for _ in range(counter):
+        timmy.color(ac)
+        timmy.forward(100)
+        timmy.right(temp)
+    counter +=1
+    
+    
+    
+
+print(timmy.pos())
+
+
+screen = Screen()
+screen.exitonclick()
